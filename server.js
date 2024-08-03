2415b6c0f6d4b3ba5efd9be1427eaf7f70adb82d
Sarthak-dhanotiya <163567015+Sarthak-dhanotiya@users.noreply.github.com>
2024-08-03 22:42:11 +0000

    node end

--- server.js
+++ server.js
@@ -1,16 +1,75 @@
 const http=require("http");
+
 const port=8081;
 
+    const arr=["hi","it ","is","server"];
+
+
 http
-.createServer((req,res)=>
-{
-    // 200 means ok
-    res.writeHead(200,{"content-type":"text/html"});
-    res.write("<h2>hey server started 123456 sartha: </h2>");
+.createServer((req,res)=>{
+    
+    const{method,url}=req;
+
+    if(url==="/hi"){
+        // GET METHOD
+        if(method==="GET"){
+            res.writeHead(200);
+            res.write(arr.toString());
+        }  // POST METHOD
+        else if(method==="POST"){
+            let body="";
+        req
+        .on("error",(err)=>{
+            console.error(err);
+})
+.on("data",(chunk)=>{
+    body+=chunk;
+
+})
+
+.on("end",()=>{
+    body=JSON.parse(body);
+
+   
+    arr.push(body.item);
+    console.log("successfully append");
+}
+)
+
+}
+// DELETE METHOD
+else if(method==="DELETE"){
+    let body="";
+    req
+    .on("error",(err)=>{
+        console.log(err);
+
+    })
+
+    .on("data",(chunk)=>{
+        body+=chunk;
+    })
+
+    .on("end",()=>{
+        body=JSON.parse(body);
+        let hi=body.item;
+        arr.find((elem,index)=>{
+            if(elem===hi){
+               arr.splice(index,1); 
+            }
+        });
+    });
+}
+
+
+      
+
+        
+    }
     res.end();
 })
-.listen(port,()=>{
-    console.log("nodejs server started running on port :${port}"
 
-    );
+.listen(port,()=>{
+    console.log(`NodeJS server started running on port :${port}`);
 });
+
